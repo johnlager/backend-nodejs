b9bfd5928e9e02f413738f053efd3d07f11513e5
johnlager <john.vitor.dss@gmail.com>
2021-09-17 16:09:38 -0300

    feat: add methods findOne and findAll to IAccountsRepository interface

--- src/modules/accounts/repository/IAccountRepository.ts
+++ src/modules/accounts/repository/IAccountRepository.ts
@@ -3,6 +3,8 @@ import { Account } from "../infra/typeorm/entity/Account";
 
 interface IAccountRepository {
   create(data: ICreateAccountDTO): Promise<Account>;
+  findOne(id: string): Promise<Account>;
+  findAll(): Promise<Account[]>;
 }
 
 export { IAccountRepository };
