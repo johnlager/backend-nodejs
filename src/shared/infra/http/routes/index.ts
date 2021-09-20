187e1faf0a09f7b228c11a60ef39717f3bc6f7b1
johnlager <john.vitor.dss@gmail.com>
2021-09-20 12:09:24 -0300

    feat: add transactions routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import { accountsRouter } from './accounts.routes'
+import { transactionsRouter } from './transactions.routes'
 
 const router = Router()
 
 router.use('/accounts', accountsRouter)
+router.use('/transactions', transactionsRouter)
 
 export { router }
