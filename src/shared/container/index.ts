0d26f12322b186d4264329154227f23bb4f38556
johnlager <john.vitor.dss@gmail.com>
2021-09-20 12:07:32 -0300

    feat: add transactions repository register in tsryringe container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,5 +1,8 @@
+import { TransactionRespository } from "@modules/transactions/infra/typeorm/repository/TransactionRepository";
+import { ITransactionRepository } from "@modules/transactions/repository/ITransactionRepository";
 import { container } from "tsyringe";
 import { AccountRespository } from "../../modules/accounts/infra/typeorm/repository/AccountRepository";
 import { IAccountRepository } from "../../modules/accounts/repository/IAccountRepository";
 
 container.registerSingleton<IAccountRepository>("AccountRepository", AccountRespository)
+container.registerSingleton<ITransactionRepository>("TransactionRepository", TransactionRespository)
